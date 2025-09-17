e16bb666237aefb2c24a2b87f78a5d8b813ff6b7
hmovaghari <hmovaghari@gmail.com>
2025-09-17 11:16:57 +0330

    add new asp.net vmc site

---
+++ hmovaghari.github.io.cs/hmovaghari.github.io.cs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hmovaghari.github.io.cs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
